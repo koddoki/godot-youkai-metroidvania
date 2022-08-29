2fea35ef0cbb6b177e17f1b31980ca4079483c8e
noah stelmak <noah.bussmann@gmail.com>
2022-08-28 22:23:12 -0300

    A

--- assets/ui/hud/CollectedItems/CollectedItems.gd
+++ assets/ui/hud/CollectedItems/CollectedItems.gd
@@ -1,18 +1,8 @@
 extends Node2D
 
 #export (PackedScene) 
-onready var woodItem = load("res://assets/objects/items/wood/WoodItem.tscn");
-onready var batata = woodItem.instance();
+# onready var woodItem = load("res://assets/objects/items/wood/WoodItem.tscn");
+
 func _ready() -> void:
-	# batata.connect("collected",self,"_on_Wood_Collected")
-	self.add_child(batata)
-	# batata.position = 
 	pass
 	
-	
-
-func _on_Wood_Collected(pos):
-	print("teste")
-	var j = woodItem.instance();
-	self.add_child(j)
-	j.position = pos
